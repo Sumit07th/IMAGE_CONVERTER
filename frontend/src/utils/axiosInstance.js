1017a1268e8edeba4541c2f68e8e94d6dd064697
Sumit Kumar <sumit6200584775@gmail.com>
2024-08-25 23:22:23 +0530

    final commit

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const BASE_URL = '/api';
+const BASE_URL = "http://localhost:5000";
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://image-converter-backend-api.vercel.app',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
 });
