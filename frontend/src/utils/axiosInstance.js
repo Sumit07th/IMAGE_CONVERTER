a500109296e07a874388993ecdb5ec42e774dba1
Sumit Kumar <108703831+Sumit07th@users.noreply.github.com>
2024-08-25 23:50:05 +0530

    Update axiosInstance.js

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const BASE_URL = "https://image-converter-backend.vercel.app";
+const BASE_URL = "https://image-converter-beige.vercel.app";
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
